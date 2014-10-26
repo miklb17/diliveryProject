897c68410808c3a7e483a13ffad809fabb4139aa
barak <baraky@gm.com>
2014-10-26 18:51:05 +0200

    add system println

---
+++ test/src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+	
+	private void syso() {
+		
+		System.out.println("HELLO REPOSITORY");
+		System.out.println("HELLO REPOSITORY");
+		System.out.println("HELLO REPOSITORY");	}
+}
