b9e5d61753422a63243be470a7ba55cedc5a5d23
boris <miklb17@gmail.com>
2014-10-26 19:43:42 +0200

    add more println

--- test/src/Test.java
+++ test/src/Test.java
@@ -5,5 +5,11 @@ public class Test {
 		
 		System.out.println("HELLO REPOSITORY");
 		System.out.println("HELLO REPOSITORY");
-		System.out.println("HELLO REPOSITORY");	}
+		System.out.println("HELLO REPOSITORY");	
+		System.out.println("HELLO REPOSITORY");
+		System.out.println("HELLO REPOSITORY");
+		System.out.println("HELLO REPOSITORY");
+		System.out.println("HELLO REPOSITORY");
+		System.out.println("HELLO REPOSITORY");
+	}
 }
